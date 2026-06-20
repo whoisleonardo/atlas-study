6417833c5f3ba80b149697bd55941798dcd9d674
whoisleo <leonardomdubena@gmail.com>
2026-06-19 21:10:22 -0300

    terraform fmt: align variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,8 @@
-variable "tenancy_ocid"     { type = string }
-variable "user_ocid"        { type = string }
-variable "fingerprint"      { type = string }
+variable "tenancy_ocid" { type = string }
+variable "user_ocid" { type = string }
+variable "fingerprint" { type = string }
 variable "private_key_path" { type = string }
-variable "region"           { type = string }
+variable "region" { type = string }
 
 variable "compartment_ocid" {
   type        = string
